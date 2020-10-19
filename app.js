97c6d4f661dcd05bd7725424a4e12da09ce9888f
evannor <erica.vannorman@gmail.com>
2020-10-18 19:37:52 -0700

    Create unique node module and minimal refactoring

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date");
 
 const app = express();
 // make let item global so that it can be accessed by all functions
@@ -13,57 +14,13 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
 
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
 
   res.render("list", {
     listTitle: day,
     newListItems: items
   });
-
-  // let currentDay = today.getDay();
-  // let day = "";
-  // check which day of the week it is
-  // switch (currentDay) {
-  //   case 0:
-  //     day = "Sunday";
-  //     break;
-  //   case 1:
-  //     day = "Monday";
-  //     break;
-  //   case 2:
-  //     day = "Tuesday";
-  //     break;
-  //   case 3:
-  //     day = "Wednesday";
-  //     break;
-  //   case 4:
-  //     day = "Thursday";
-  //     break;
-  //   case 5:
-  //     day = "Friday";
-  //     break;
-  //   case 6:
-  //     day = "Saturday";
-  //     break;
-  //   default:
-  //     // checks for errors, writes log in cmd line
-  //     console.log("Error: currentDay day is equal to: " + currentDay);
-  // }
-
-  // checking to see if it is a weekend
-  // if(currentDay === 6 || currentDay === 0) {
-  //   day = "Weekend";
-  // } else {
-  //   day = "Weekday";
-  // }
-  // res.send("Server is up and running");
 });
 
 app.post("/", function(req, res) {
@@ -75,8 +32,6 @@ app.post("/", function(req, res) {
     items.push(item);
     res.redirect("/");
   }
-  // console.log("The user entered: " + req.body.newItem);
-  // need to fully initialize body-parser to get to the form submission
 });
 
 app.get("/work", function(req, res) {
